2e9d2172bf68ab78282d91c0534727184fbb253a
Guilherme Ytalo <guilherme.azevedo@bitzen.com.br>
2021-01-22 09:12:59 -0300

    Class2

---
+++ class2/lib/main.dart
@@ -0,0 +1,80 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Master Class 2'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int _randomNumber = 0;
+
+  List _phrases = [
+    'Bom Dia que deus te elimine',
+    'O dia de hoje foi ruim mas o de amanhã será pior',
+    'Quer me derrubar? logo eu que já nasci no chão',
+    'Um dia ruim pra quem tem uma vida fodida é um dia normal'
+  ];
+
+  void _incrementCounter() {
+    setState(() {
+
+      _randomNumber = new Random().nextInt(4); // 0
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Pressione o botão para gerar uma frase inspiradora:',
+            ),
+            Text(
+              _phrases[_randomNumber],
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
